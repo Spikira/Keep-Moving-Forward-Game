dee0bfb1a2351829182aed9b5ac7f304d5654131
Spikira <103157374+Spikira@users.noreply.github.com>
2022-10-05 23:26:09 +0700

    bug hunting

--- data/enemies/purple_sludge.lua
+++ data/enemies/purple_sludge.lua
@@ -10,16 +10,16 @@ function enemy:on_created()
   sprite = enemy:create_sprite("enemies/" .. enemy:get_breed())
   enemy:set_life(5)
   enemy:set_damage(1)
-  m = sol.movement.create("circle")
-  -- m:set_speed(42)
-  m:set_radius(16)
-  m:start(enemy)
 end
 
 -- Event called when the enemy should start or restart its movements.
 -- This is called for example after the enemy is created or after
 -- it was hurt or immobilized.
 function enemy:on_restarted()
+  m = sol.movement.create("circle")
+  -- m:set_speed(42)
+  m:set_radius(16)
+  m:start(enemy)
 end
 
 function enemy:on_dying()
