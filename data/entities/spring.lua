338fb30534ed80e88954b11a8ab21d25476227f7
Spikira <103157374+Spikira@users.noreply.github.com>
2022-10-10 21:08:55 +0700

    ACT 1 COMPLETE

--- data/entities/spring.lua
+++ data/entities/spring.lua
@@ -9,13 +9,7 @@ function entity:on_created()
   entity:add_collision_test("sprite", function(entity_a, entity_b)
     if entity_b:get_type() == "hero" then
       local x, y, layer = entity_b:get_position()
-      for a = 1, 0, 11 do
-        if entity_b:test_obstacles(x, y - 88 + (8 * a), layer) == false then
-          entity_b:set_position(x, y - 88 + (8 * a), layer)
-        else
-          a = a + 1
-        end
-      end
+      entity_b:set_position(x, y + 88, layer)
       sprite:set_animation("spring", function()
         sprite:set_animation("stopped")
       end)
